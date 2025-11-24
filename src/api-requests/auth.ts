2c8494c044f4bd16ec22e68f2b24f18dd28a7c5e
Chainuocngot <anhvu.dev.103@gmail.com>
2025-11-25 00:48:52 +0700

    Bài 27: Code route handler logout

--- src/api-requests/auth.ts
+++ src/api-requests/auth.ts
@@ -1,10 +1,38 @@
 import http from "@/lib/http"
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema"
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+} from "@/schemaValidations/auth.schema"
+import { MessageResType } from "@/schemaValidations/common.schema"
 
 const authApiRequests = {
+  //Login
   sLogin: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body),
   cLogin: (body: LoginBodyType) =>
     http.post<LoginResType>("/api/auth/login", body, {
+      baseUrl: "",
+    }),
+
+  //Logout
+  sLogout: (
+    body: LogoutBodyType & {
+      accessToken: string
+    },
+  ) =>
+    http.post<MessageResType>(
+      "/auth/logout",
+      {
+        refreshToken: body.refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`,
+        },
+      },
+    ),
+  cLogout: () =>
+    http.post<MessageResType>("/api/auth/logout", null, {
       baseUrl: "",
     }),
 }
