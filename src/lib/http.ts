df49f7fca30d7ac20cc3dfe7c015fbaa48ae168f
Chainuocngot <anhvu.dev.103@gmail.com>
2025-11-24 16:41:13 +0700

    Bài 20: Tạo http client để gọi API

---
+++ src/lib/http.ts
@@ -0,0 +1,192 @@
+import envConfig from "@/config"
+import { normalizePath } from "@/lib/utils"
+import { LoginResType } from "@/schemaValidations/auth.schema"
+import { redirect } from "next/navigation"
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type CustomRequestInit = Omit<RequestInit, "method"> & {
+  baseUrl?: string | undefined
+}
+
+const ENTITY_ERROR_STATUS = 422
+const AUTHENTICATION_ERROR_STATUS = 401
+
+type EntityErrorPayload = {
+  message: string
+  errors: {
+    field: string
+    message: string
+  }[]
+}
+
+export class HttpError extends Error {
+  status: number
+  payload: {
+    message: string
+    [key: string]: any
+  }
+
+  constructor({
+    status,
+    payload,
+    message = "Lỗi Http",
+  }: {
+    status: number
+    payload: any
+    message?: string
+  }) {
+    super(message)
+    this.status = status
+    this.payload = payload
+  }
+}
+
+export class EntityError extends HttpError {
+  status: typeof ENTITY_ERROR_STATUS
+  payload: EntityErrorPayload
+
+  constructor(payload: EntityErrorPayload) {
+    super({
+      status: ENTITY_ERROR_STATUS,
+      payload,
+      message: "Lỗi thực thể",
+    })
+
+    this.status = ENTITY_ERROR_STATUS
+    this.payload = payload
+  }
+}
+
+let clientLogoutRequest: null | Promise<any> = null
+
+const isClient = typeof window !== "undefined"
+
+const request = async <Response>(
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  url: string,
+  options: CustomRequestInit | undefined,
+) => {
+  let body: FormData | string | undefined = undefined
+
+  if (options?.body instanceof FormData) {
+    body = options.body
+  } else if (options?.body) {
+    body = JSON.stringify(options.body)
+  }
+
+  const baseHeaders: {
+    [key: string]: string
+  } =
+    body instanceof FormData
+      ? {}
+      : {
+          "Content-Type": "application/json",
+        }
+
+  if (isClient) {
+    const accessToken = localStorage.getItem("accessToken")
+
+    if (accessToken) {
+      baseHeaders["Authorization"] = `Bearer ${accessToken}`
+    }
+  }
+
+  const baseUrl =
+    options?.baseUrl === undefined
+      ? envConfig.NEXT_PUBLIC_API_ENDPOINT
+      : options.baseUrl
+
+  const fullUrl = `${baseUrl}/${normalizePath(url)}`
+  const res = await fetch(fullUrl, {
+    ...options,
+    headers: { ...baseHeaders, ...options?.headers } as any,
+    body,
+    method,
+  })
+  const payload: Response = await res.json()
+  const data = {
+    status: res.status,
+    payload,
+  }
+
+  if (!res.ok) {
+    if (res.status === ENTITY_ERROR_STATUS) {
+      throw new EntityError(data.payload as EntityErrorPayload)
+    } else if (res.status === AUTHENTICATION_ERROR_STATUS) {
+      if (isClient) {
+        if (!clientLogoutRequest) {
+          clientLogoutRequest = fetch("/api/auth/logout", {
+            method: "POST",
+            body: null,
+            headers: {
+              ...baseHeaders,
+            } as any,
+          })
+
+          try {
+            await clientLogoutRequest
+          } catch {
+          } finally {
+            localStorage.removeItem("accessToken")
+            localStorage.removeItem("refreshToken")
+            clientLogoutRequest = null
+            location.href = "/login"
+          }
+        }
+      } else {
+        const accessToken = (options?.headers as any).Authorization.split(
+          "Bearer ",
+        )[1]
+        redirect(`/logout?accessToken=${accessToken}`)
+      }
+    } else {
+      throw new HttpError(data)
+    }
+  }
+
+  if (isClient) {
+    const normalUrl = normalizePath(url)
+    if (normalUrl === "api/auth/login") {
+      const { accessToken, refreshToken } = (payload as LoginResType).data
+
+      localStorage.setItem("accessToken", accessToken)
+      localStorage.setItem("refreshToken", refreshToken)
+    } else if ("api/auth/logout" === normalUrl) {
+      localStorage.removeItem("accessToken")
+      localStorage.removeItem("refreshToken")
+    }
+  }
+
+  return data
+}
+
+const http = {
+  get<Response>(
+    url: string,
+    options?: Omit<CustomRequestInit, "body"> | undefined,
+  ) {
+    return request<Response>("GET", url, options)
+  },
+  post<Response>(
+    url: string,
+    body: any,
+    options?: Omit<CustomRequestInit, "body"> | undefined,
+  ) {
+    return request<Response>("POST", url, { ...options, body })
+  },
+  put<Response>(
+    url: string,
+    body: any,
+    options?: Omit<CustomRequestInit, "body"> | undefined,
+  ) {
+    return request<Response>("PUT", url, { ...options, body })
+  },
+  delete<Response>(
+    url: string,
+    options?: Omit<CustomRequestInit, "body"> | undefined,
+  ) {
+    return request<Response>("DELETE", url, options)
+  },
+}
+
+export default http
