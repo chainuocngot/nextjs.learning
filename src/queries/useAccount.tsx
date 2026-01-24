d0bb154a5dedc4e90b97ea421a124e6769645885
dvanh.hxg <anhvu.dev.103@gmail.com>
2026-01-24 22:14:21 +0700

    Bài 47: Khai báo API cho accounts

--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,5 +1,6 @@
 import accountApiRequests from "@/api-requests/account"
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema"
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query"
 
 export const useAccountMe = () => {
   return useQuery({
@@ -17,5 +18,63 @@ export const useUpdateMeMutation = () => {
 export const useChangePasswordMutation = () => {
   return useMutation({
     mutationFn: accountApiRequests.changePassword,
+  })
+}
+
+export const useGetAccountList = () => {
+  return useQuery({
+    queryKey: ["accounts"],
+    queryFn: accountApiRequests.list,
+  })
+}
+
+export const useGetAccount = ({ id }: { id: number }) => {
+  return useQuery({
+    queryKey: ["accounts", id],
+    queryFn: () => accountApiRequests.getEmployee(id),
+  })
+}
+
+export const useAddAccountMutation = () => {
+  const queryClient = new QueryClient()
+
+  return useMutation({
+    mutationFn: accountApiRequests.addEmployee,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["accounts"],
+      })
+    },
+  })
+}
+
+export const useUpdateAccountMutation = () => {
+  const queryClient = new QueryClient()
+
+  return useMutation({
+    mutationFn: ({
+      id,
+      ...body
+    }: UpdateEmployeeAccountBodyType & {
+      id: number
+    }) => accountApiRequests.updateEmployee(id, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["accounts"],
+      })
+    },
+  })
+}
+
+export const useDeleteAccountMutation = () => {
+  const queryClient = new QueryClient()
+
+  return useMutation({
+    mutationFn: accountApiRequests.deleteEmployee,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["accounts"],
+      })
+    },
   })
 }
