028ab2dd0dd540f041089bbfd0cf7a8979de7a8d
dvanh.hxg <anhvu.dev.103@gmail.com>
2026-01-28 00:08:31 +0700

    Bài 52: Xoá account

--- src/app/manage/accounts/account-table.tsx
+++ src/app/manage/accounts/account-table.tsx
@@ -53,7 +53,12 @@ import {
 } from "@/components/ui/alert-dialog"
 import { useSearchParams } from "next/navigation"
 import AutoPagination from "@/components/auto-pagination"
-import { useGetAccountList } from "@/queries/useAccount"
+import {
+  useDeleteAccountMutation,
+  useGetAccountList,
+} from "@/queries/useAccount"
+import { handleErrorApi } from "@/lib/utils"
+import { toast } from "sonner"
 
 type AccountItem = AccountListResType["data"][0]
 
@@ -149,6 +154,22 @@ function AlertDialogDeleteAccount({
   employeeDelete: AccountItem | null
   setEmployeeDelete: (value: AccountItem | null) => void
 }) {
+  const deleteAccountMutation = useDeleteAccountMutation()
+
+  const deleteAccount = async () => {
+    if (employeeDelete) {
+      try {
+        const result = await deleteAccountMutation.mutateAsync(
+          employeeDelete.id,
+        )
+        setEmployeeDelete(null)
+        toast.success(result.payload.message)
+      } catch (error) {
+        handleErrorApi({ error })
+      }
+    }
+  }
+
   return (
     <AlertDialog
       open={Boolean(employeeDelete)}
@@ -171,7 +192,9 @@ function AlertDialogDeleteAccount({
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={deleteAccount}>
+            Continue
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
