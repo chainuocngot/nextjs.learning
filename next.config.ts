28702200c405446a31a70fb59f72307d8fecbe38
dvanh.hxg <anhvu.dev.103@gmail.com>
2026-02-08 01:30:56 +0700

    Bài 66: Fetch danh sách món ăn ở trang chủ

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     qualities: [75, 100],
+    dangerouslyAllowLocalIP: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
